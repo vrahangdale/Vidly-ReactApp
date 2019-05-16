1589ddec55962d9914754bdc6fe44b120da62a0f
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2019-05-16 00:58:57 -0400

    finished the movie table

--- src/componets/Movies.jsx
+++ src/componets/Movies.jsx
@@ -5,28 +5,52 @@ class Movies extends Component {
   state = { movies: getMovies() };
   render() {
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map(movie => (
+      <div>
+        <div className="badge">{this.movieCount()}</div>
+        <table className="table">
+          <thead>
             <tr>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
+  movieCount = () => {
+    if (this.state.movies.length === 0) {
+      return "There is no movies in the database";
+    }
+    let count = this.state.movies.length;
+    return "There are " + count + " in the Database";
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
 }
 
 export default Movies;
