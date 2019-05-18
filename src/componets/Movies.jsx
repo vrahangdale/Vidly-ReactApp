8ba0f54d8eed4592b67e4b644a18e567e923d489
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2019-05-17 23:40:52 -0400

    aaded the like component and used it in movies component

--- src/componets/Movies.jsx
+++ src/componets/Movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/Like";
 
 class Movies extends Component {
   state = { movies: getMovies() };
@@ -14,6 +15,7 @@ class Movies extends Component {
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
+              <th>Like</th>
               <th />
             </tr>
           </thead>
@@ -24,6 +26,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
@@ -49,6 +57,13 @@ class Movies extends Component {
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
 }
---
+++ src/componets/common/Like.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+    />
+  );
+};
+
+export default Like;
